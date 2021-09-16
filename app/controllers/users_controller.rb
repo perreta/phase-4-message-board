3cb2b27b64aba8943e41845a8009acaf6613b5b4
perreta <84996440+perreta@users.noreply.github.com>
2021-09-16 08:48:40 -0400

    something

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.create(user_params)
+        user = User.create!(user_params)
         session[:user_id] = user.id
         render json: user, status: :created 
     end 
