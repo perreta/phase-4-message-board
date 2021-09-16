50936922a2c2311e539c8a242a1e727bde0d1b81
perreta <84996440+perreta@users.noreply.github.com>
2021-09-16 09:27:28 -0400

    fixed timezone

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -4,10 +4,10 @@ class PostSerializer < ActiveModel::Serializer
   belongs_to :user
 
   def date
-    object.created_at.strftime("%b %e,  %l:%M %p")
+    object.created_at.localtime.strftime("%b %e,  %l:%M %p")
   end
 
   def updated_date
-    object.updated_at.strftime("%b %e,  %l:%M %p")
+    object.updated_at.localtime.strftime("%b %e,  %l:%M %p")
   end
 end
